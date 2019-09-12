aaa7adad10680c472923786e490c829a55a70c90
Marcello Stanisci <stanisci.m@gmail.com>
2019-09-12 16:52:18 +0200

    Copyright header.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+/**
+ * This file is part of LIBEUFIN.
+ * Copyright (C) 2019 Stanisci and Dold.
+
+ * LIBEUFIN is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3, or
+ * (at your option) any later version.
+
+ * LIBEUFIN is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+ * Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public
+ * License along with LIBEUFIN; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>
+ */
+
 package tech.libeufin
 
 import io.ktor.application.*
