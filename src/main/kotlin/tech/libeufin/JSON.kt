9f97caddceb2ff30222d4dfef79ecb4cc716c369
Marcello Stanisci <stanisci.m@gmail.com>
2019-09-30 17:02:32 +0200

    JSON types for INI/HIA letters

--- src/main/kotlin/tech/libeufin/JSON.kt
+++ src/main/kotlin/tech/libeufin/JSON.kt
@@ -3,7 +3,7 @@ package tech.libeufin
 /**
  * Error message.
  */
-data class SandboxError (
+data class SandboxError(
     val message: String
 )
 
@@ -11,22 +11,59 @@ data class SandboxError (
 /**
  * Request for POST /admin/customers
  */
-data class CustomerRequest (
+data class CustomerRequest(
     val name: String
 )
 
-data class CustomerResponse (
+data class CustomerResponse(
     val id: Int
 )
 
 /**
  * Response for GET /admin/customers/:id
  */
-data class CustomerInfo (
+data class CustomerInfo(
     val name: String,
     val customerEbicsInfo: CustomerEbicsInfo
 )
 
-data class CustomerEbicsInfo (
+data class CustomerEbicsInfo(
     val userId: Int
+)
+
+/**
+ * Request for INI / HIA letter(s).
+ */
+data class IniHiaLetters(
+
+    val userId: String,
+    val customerId: String,
+    val name: String,
+    val date: String,
+    val time: String,
+    val recipient: String,
+    val exponent: String,
+    val modulus: String,
+    val hash: String,
+    val INI: IniVersion,
+    val HIA: HiaVersion
+)
+
+/**
+ * INI specific version numbers
+ */
+data class IniVersion(
+    // Signature key
+    val es_version: String
+)
+
+/**
+ * INI specific version numbers
+ */
+data class HiaVersion(
+
+    // Identification and authentication key
+    val ia_version: String,
+    // Encryption key
+    val enc_version: String
 )
