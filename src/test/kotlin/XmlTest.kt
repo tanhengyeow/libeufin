a7f51ef785d155b1416e9b99088b163ee14c857c
Marcello Stanisci <stanisci.m@gmail.com>
2019-10-07 16:54:09 +0200

    test skeleton

---
+++ src/test/kotlin/XmlTest.kt
@@ -0,0 +1,12 @@
+package tech.libeufin
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.Assert.*
+
+class XmlTest {
+    @Test
+    fun iniValidation(){
+        assert(false)
+    }
+}
