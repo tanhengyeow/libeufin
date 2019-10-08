26389ed2f7fd3edacf6b9ad0515b212627f06ce4
Marcello Stanisci <stanisci.m@gmail.com>
2019-10-08 17:17:15 +0200

    include logback configuration

--- sandbox/src/test/kotlin/LogTest.kt
+++ sandbox/src/test/kotlin/LogTest.kt
@@ -14,7 +14,7 @@ class LogTest {
         val cl = ClassLoader.getSystemClassLoader()
         val urls = (cl as URLClassLoader).urLs
         for (url in urls) {
-            // println(url)
+            println(url)
         }
         val logger = LoggerFactory.getLogger("sandbox.log.test")
         logger.info("line")
