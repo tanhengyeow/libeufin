2d0b2e24f0e91c16a479bc2e8545a4d2461d9ce4
Marcello Stanisci <stanisci.m@gmail.com>
2019-10-08 16:55:47 +0200

    get logback.xml picked up

--- sandbox/src/test/kotlin/LogTest.kt
+++ sandbox/src/test/kotlin/LogTest.kt
@@ -4,11 +4,18 @@ import org.junit.Assert
 import org.junit.Test
 import org.junit.Assert.*
 import org.slf4j.LoggerFactory
+import java.net.URLClassLoader
 
 class LogTest {
 
     @Test
     fun logLine() {
+
+        val cl = ClassLoader.getSystemClassLoader()
+        val urls = (cl as URLClassLoader).urLs
+        for (url in urls) {
+            // println(url)
+        }
         val logger = LoggerFactory.getLogger("sandbox.log.test")
         logger.info("line")
     }
