cffa45651040778354c73ddcaa4c51ee3725b6ad
Marcello Stanisci <stanisci.m@gmail.com>
2019-10-08 18:04:42 +0200

    attempt to turn logging off for io.ktor.*

--- sandbox/src/test/kotlin/LogTest.kt
+++ sandbox/src/test/kotlin/LogTest.kt
@@ -11,11 +11,6 @@ class LogTest {
     @Test
     fun logLine() {
 
-        val cl = ClassLoader.getSystemClassLoader()
-        val urls = (cl as URLClassLoader).urLs
-        for (url in urls) {
-            // println(url)
-        }
         val logger = LoggerFactory.getLogger("sandbox.log.test")
         logger.info("line")
     }
