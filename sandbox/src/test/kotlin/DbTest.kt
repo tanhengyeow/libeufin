60b65886f825057a74e208de9a345c61115c128d
Marcello Stanisci <stanisci.m@gmail.com>
2019-10-17 16:08:14 +0200

    introducing nullable references

---
+++ sandbox/src/test/kotlin/DbTest.kt
@@ -0,0 +1,35 @@
+package tech.libeufin.sandbox
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.Before
+
+class DbTest {
+
+    @Before
+    fun setUp() {
+        dbCreateTables()
+    }
+
+    /**
+     * This function creates a EBICS subscriber _first_, and
+     * subsequently tries to insert a mock bianry value into
+     * the keys columns of the subscriber.
+     */
+    @Test
+    fun storeBinary() {
+        transaction {
+            // join table
+            val subscriber = createSubscriber()
+
+            val key = EbicsPublicKey.new {
+                pub = "BINARYVALUE".toByteArray()
+                state = KeyStates.NEW
+            }
+
+            subscriber.authorizationKey = key
+        }
+    }
+}
