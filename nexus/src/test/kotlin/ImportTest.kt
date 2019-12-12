66e46bd3b4824458ddcf537616670dec7ee58f8c
Marcello Stanisci <stanisci.m@gmail.com>
2019-12-12 14:48:13 +0100

    shared module skeleton

---
+++ nexus/src/test/kotlin/ImportTest.kt
@@ -0,0 +1,12 @@
+package tech.libeufin.nexus
+
+import org.junit.Test
+import tech.libeufin.util.IMPORT
+
+class ImportTest {
+
+    @Test
+    fun importTest() {
+        println(IMPORT)
+    }
+}
---
+++ util/src/main/kotlin/ImportTest.kt
@@ -0,0 +1,3 @@
+package tech.libeufin.util
+
+const val IMPORT = "third party"
