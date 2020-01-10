0343e36936beb31d0aeb3a4bf712d3844eec4f1f
Marcello Stanisci <stanisci.m@gmail.com>
2020-01-10 15:44:23 +0100

    debug

--- nexus/src/main/kotlin/DB.kt
+++ nexus/src/main/kotlin/DB.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
+
 object EbicsSubscribersTable : IntIdTable() {
     val ebicsURL = text("ebicsURL")
     val hostID = text("hostID")
