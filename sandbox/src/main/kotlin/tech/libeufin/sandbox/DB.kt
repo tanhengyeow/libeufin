871addea7ca77d619cc70097dc1be2b72499f0da
Marcello Stanisci <stanisci.m@gmail.com>
2020-01-11 12:51:29 +0100

    enabling SQLite

--- sandbox/src/main/kotlin/tech/libeufin/sandbox/DB.kt
+++ sandbox/src/main/kotlin/tech/libeufin/sandbox/DB.kt
@@ -360,8 +360,8 @@ class EbicsUploadTransactionChunkEntity(id : EntityID<String>): Entity<String>(i
 
 
 fun dbCreateTables() {
-    // Database.connect("jdbc:sqlite:libeufin-sandbox.sqlite3", "org.sqlite.JDBC")
-    Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+    Database.connect("jdbc:sqlite:libeufin-sandbox.sqlite3", "org.sqlite.JDBC")
+    // Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
 
     transaction {
