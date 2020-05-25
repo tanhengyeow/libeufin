3db2403f55914a2251afbd48abae95c161a16ee5
MS <ms@taler.net>
2020-05-25 17:04:40 +0200

    LocalDateTime->ZonedDateTime

--- util/src/test/kotlin/TimeTest.kt
+++ util/src/test/kotlin/TimeTest.kt
@@ -1,11 +1,8 @@
 import org.junit.Test
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.*
+import java.time.format.DateTimeFormatter
 
 class TimeTest {
-
     @Test
     fun importMillis() {
         fun fromLong(millis: Long): LocalDateTime {
@@ -16,5 +13,21 @@ class TimeTest {
         }
         val ret = fromLong(0)
         println(ret.toString())
+    }
+
+    @Test
+    fun formatDateTime() {
+        fun formatDashed(dateTime: LocalDateTime): String {
+            val dtf = DateTimeFormatter.ISO_LOCAL_DATE
+            return dtf.format(dateTime)
+        }
+        fun formatZonedWithOffset(dateTime: ZonedDateTime): String {
+            val dtf = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+            return dtf.format(dateTime)
+        }
+        val str = formatDashed(LocalDateTime.now())     
+        println(str)
+        val str0 = formatZonedWithOffset(LocalDateTime.now().atZone(ZoneId.systemDefault()))
+        println(str0)
     }
 }
