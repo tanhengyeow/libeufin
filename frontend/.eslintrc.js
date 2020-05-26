1bd464c184a177311c399eb858a87bad1667c4cb
tanhengyeow <E0032242@u.nus.edu>
2020-05-26 20:32:15 +0800

    Added new dependencies and did some housekeeping

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: ["airbnb-typescript-prettier"],
+  extends: ['airbnb-typescript-prettier'],
   rules: {
     'react/prop-types': 0,
     'react/destructuring-assignment': 0,
     'react/static-property-placement': 0,
     'jsx-a11y/alt-text': 0,
     'react/jsx-props-no-spreading': 0,
+    'jsx-a11y/label-has-associated-control': 0,
   },
 };
