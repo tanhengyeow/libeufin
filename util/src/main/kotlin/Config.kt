6044b5c2fd83632eb1441545cb1d427a74e98b0a
MS <ms@taler.net>
2020-06-24 11:52:08 +0200

    nexus offers logfile name among options

---
+++ util/src/main/kotlin/Config.kt
@@ -0,0 +1,20 @@
+import ch.qos.logback.classic.util.ContextInitializer
+import ch.qos.logback.core.util.Loader
+
+/**
+ * Set system properties to wanted values, and load logback configuration after.
+ * While it can set any system property, it is used only to set the log file name.
+ *
+ * @param logFile filename of logfile.  If null, then no logfile will be produced.
+ * @param logFileNameAsProperty property that indicates the logfile name in logback configuration.
+ * @param configFileName name of logback's config file.  Typically something different
+ * from "logback.xml" (otherwise logback will load it by itself upon startup.)
+ */
+fun setLogFile(logFile: String?, logFileNameAsProperty: String, configFileName: String) {
+    if (logFile != null) System.setProperty(logFileNameAsProperty, logFile)
+    val configFilePath = Loader.getResource(configFileName, ClassLoader.getSystemClassLoader())
+    if (configFilePath == null) {
+        println("Warning: could not find log config file")
+    }
+    System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, configFilePath.toString())
+}
