89cf52ad1d9f8ff130fb9e5499e51b7129c32dd9
MS <ms@taler.net>
2020-06-29 15:35:59 +0200

    comment

--- nexus/src/test/kotlin/DBTest.kt
+++ nexus/src/test/kotlin/DBTest.kt
@@ -52,6 +52,7 @@ class DBTest {
                     it[col1] = "foo"
                     it[col2] = "bar"
                 }
+                MyTable.insert {  } // shouldn't it fail for non-NULL constraint violation?
             }
         }
     }
