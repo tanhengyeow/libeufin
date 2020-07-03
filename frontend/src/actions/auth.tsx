1bbf0419eb957881cebc19e5ae854a04116c7c10
tanhengyeow <E0032242@u.nus.edu>
2020-07-03 12:57:56 +0800

    Update status code for invalid credentials

--- frontend/src/actions/auth.tsx
+++ frontend/src/actions/auth.tsx
@@ -34,7 +34,7 @@ export const login = (nexusURL: string, username: string, password: string) => {
         .then((response) => {
           if (response.ok) {
             return response.json();
-          } else if (response.status === 401) {
+          } else if (response.status === 403) {
             throw new Error('Invalid credentials');
           }
           throw new Error('Cannot connect to server');
